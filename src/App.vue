7948e6faa162db017eac90e43f9522119c96d548
Dikov <s.d.dikov@gmail.com>
2019-12-15 19:50:28 +0300

    new search

--- src/App.vue
+++ src/App.vue
@@ -5,17 +5,28 @@
   </header>
   <main>
     <section class="cards-section container pt-4">
-      <input v-if="!errored" v-model.lazy="filter" v-debounce="500"  class="main-search form-control mb-3" placeholder="Search characters">
+      <input v-if="!errored" v-model.lazy="filter" v-on:change="getFirstPage" v-debounce="500" class="main-search form-control mb-3" placeholder="Search characters">
       <div v-if="errored" class="alert alert-danger" role="alert">
         We're sorry, we're not able to retrieve this information at the moment, please try back later
       </div>
       <div v-if="loading" class="d-flex justify-content-center">
-        <div class="spinner-border text-light" style="width: 6rem; height: 6rem;" role="status">
+        <div class="spinner-border text-light people-spinner" role="status">
           <span class="sr-only">Loading...</span>
         </div>
       </div>
-      <app-people-list v-if="!errored" v-bind:filter="filter" v-bind:people="characters">
+      <app-people-list v-if="!errored && !loading" v-bind:people="characters">
       </app-people-list>
+      <nav v-if="!errored" aria-label="Page navigation" class="d-flex justify-content-center mb-4">
+        <button type="button" class="btn btn-secondary btn-prev" v-bind:disabled="pageNumber <= 1" v-on:click="previousPage">
+          Previous
+        </button>
+        <div class="btn-group flex-wrap justify-content-center" role="group" aria-label="pagination">
+          <button v-for="page in pageCount" v-bind:key="page.id" v-on:click="changePage(page)" v-bind:disabled="pageNumber === page" type="button" class="btn btn-secondary btn-number">{{ page }}</button>
+        </div>
+        <button type="button" class="btn btn-secondary btn-next" v-bind:disabled="pageNumber >= pageCount" v-on:click="nextPage">
+          Next
+        </button>
+      </nav>
     </section>
   </main>
 </div>
@@ -27,6 +38,9 @@ import axios from 'axios'
 export default {
   data() {
     return {
+      charactersQuantity: 10,
+      pageNumber: 1,
+      cardsOnPage: 10,
       characters: [],
       filter: '',
       errored: false,
@@ -34,6 +48,68 @@ export default {
     }
   },
   methods: {
+    getFirstPage(){
+      this.pageNumber = 1
+      this.getCharacters()
+    },
+    getSearch() {
+      axios
+        .get('https://swapi.co/api/people/?search=' + this.filter + '&page=' + this.pageNumber)
+        .then(response => {
+          this.charactersQuantity = response.data.count
+          this.characters = response.data.results
+          this.characters.forEach(item => {
+            this.getSpecies(item)
+          })
+        })
+        /*eslint-disable no-unused-vars*/
+        .catch(error => {
+          this.errored = true
+        })
+        /*eslint-enable no-unused-vars*/
+        .finally(() => {
+          this.loading = false
+        })
+    },
+    getRequest(){
+      axios
+        .get('https://swapi.co/api/people/?page=' + this.pageNumber)
+        .then(response => {
+          this.charactersQuantity = response.data.count
+          this.characters = response.data.results
+          this.characters.forEach(item => {
+            this.getSpecies(item)
+          })
+        })
+        /*eslint-disable no-unused-vars*/
+        .catch(error => {
+          this.errored = true
+        })
+        /*eslint-enable no-unused-vars*/
+        .finally(() => {
+          this.loading = false
+        })
+    },
+    getCharacters() {
+      this.loading = true
+      if (this.filter) {
+        this.getSearch()
+      } else {
+        this.getRequest()
+      }
+    },
+    nextPage() {
+      this.pageNumber++
+      this.getCharacters()
+    },
+    previousPage() {
+      this.pageNumber--
+      this.getCharacters()
+    },
+    changePage(number) {
+      this.pageNumber = number
+      this.getCharacters()
+    },
     getSpecies(i) {
       if (i.species[0]) {
         axios.get(i.species[0])
@@ -46,32 +122,12 @@ export default {
     }
   },
   mounted() {
-    axios.all([
-        axios.get('https://swapi.co/api/people/'),
-        axios.get('https://swapi.co/api/people/?page=2'),
-        axios.get('https://swapi.co/api/people/?page=3'),
-        axios.get('https://swapi.co/api/people/?page=4'),
-        axios.get('https://swapi.co/api/people/?page=5'),
-        axios.get('https://swapi.co/api/people/?page=6'),
-        axios.get('https://swapi.co/api/people/?page=7'),
-        axios.get('https://swapi.co/api/people/?page=8'),
-        axios.get('https://swapi.co/api/people/?page=9'),
-      ])
-      .then(axios.spread((page_1, page_2, page_3, page_4, page_5, page_6, page_7, page_8, page_9) => {
-        this.characters = page_1.data.results.concat(page_2.data.results, page_3.data.results,
-          page_4.data.results, page_5.data.results, page_6.data.results, page_7.data.results,
-          page_8.data.results, page_9.data.results)
-        this.characters.forEach(item => {
-          this.getSpecies(item)
-        })
-      }))
-      /*eslint-disable no-unused-vars*/
-      .catch(error => {this.errored = true
-      })
-      /*eslint-enable no-unused-vars*/
-      .finally(() => {
-        this.loading = false
-      })
+    this.getCharacters()
+  },
+  computed: {
+    pageCount() {
+      return Math.ceil(this.charactersQuantity / this.cardsOnPage)
+    }
   }
 }
 </script>
@@ -119,5 +175,25 @@ body {
 .cards-list .card {
   border-color: rgba(52, 58, 64, 1);
   background-color: rgba(52, 58, 64, 0.8);
+}
+
+.people-spinner {
+  width: 6rem;
+  height: 6rem;
+}
+
+.btn-prev {
+  max-width: 86px;
+  max-height: 38px;
+}
+
+.btn-next {
+  max-width: 60px;
+  max-height: 38px;
+}
+
+.btn-number {
+  max-width: 44px;
+  max-height: 38px;
 }
 </style>
--- src/components/PeopleList.vue
+++ src/components/PeopleList.vue
@@ -1,7 +1,7 @@
 <template>
 <div>
-  <div class="row cards-list mb-3 justify-content-center" style="min-height: 514px; min-width: 290px">
-    <div class="card col-md-3 m-1" v-for="person in portion" v-bind:key="person.id">
+  <div class="row cards-list mb-3 justify-content-center">
+    <div class="card col-md-3 m-1" v-for="person in people" v-bind:key="person.id">
       <div class="card-body">
         <h5 class="card-title">{{ person.name }}</h5>
         <h6 class="card-subtitle mb-2">Species: {{ person.species }}</h6>
@@ -10,75 +10,14 @@
       </div>
     </div>
   </div>
-  <nav aria-label="Page navigation" class="d-flex justify-content-center mb-4">
-    <button style="max-width: 86px; max-height: 38px;" type="button" class="btn btn-secondary" v-bind:disabled="pageNumber <= 1" v-on:click="previousPage">
-      Previous
-    </button>
-    <div class="btn-group flex-wrap justify-content-center" role="group" aria-label="pagination">
-      <button style="max-width: 44px; max-height: 38px;" v-for="page in pageCount" v-bind:key="page.id" v-on:click="changePage(page)" v-bind:disabled="pageNumber === page" type="button" class="btn btn-secondary">{{ page }}</button>
-    </div>
-    <button style="max-width: 60px; max-height: 38px;" type="button" class="btn btn-secondary" v-bind:disabled="pageNumber >= pageCount" v-on:click="nextPage">
-      Next
-    </button>
-  </nav>
 </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      pageNumber: 1
-    }
-  },
   props: {
     people: {
       required: true
-    },
-    filter: {
-      type: String,
-      required: false
-    },
-    cardsOnPage: {
-      type: Number,
-      required: false,
-      default: 9
-    }
-  },
-  methods: {
-    nextPage() {
-      this.pageNumber++
-    },
-    previousPage() {
-      this.pageNumber--
-    },
-    changePage(number) {
-      this.pageNumber = number
-    },
-    getFirstPage() {
-      if (this.pageCount < this.pageNumber) {
-        this.pageNumber = 1
-      }
-    }
-  },
-  computed: {
-    pageCount() {
-      var pages = Math.ceil(this.filteredData.length / this.cardsOnPage)
-      return pages
-    },
-    filteredData() {
-      var lowCaseSearch = this.filter.toLowerCase()
-      return this.people.filter(function(i) {
-        return Object.values(i).some(function(j) {
-          return j.toString().toLowerCase().includes(lowCaseSearch)
-        })
-      })
-    },
-    portion() {
-      this.getFirstPage()
-      var start = (this.cardsOnPage * this.pageNumber) - this.cardsOnPage
-      var end = start + this.cardsOnPage
-      return this.filteredData.slice(start, end)
     }
   }
 }
